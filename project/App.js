b0c3b473b90d7ff968ddab93f02b87c4ca8ef362
Ignacio Fernandez Lemos <ignacio.f.l@hotmail.com>
2023-12-08 15:44:05 -0300

    chore: organize files

--- project/App.js
+++ project/App.js
@@ -1,43 +1,8 @@
 import express from "express"
+import {students} from "./Students.js"
 
 const server = express();
 server.use(express.urlencoded({extended: true}));
-
-const students = [
-    {
-        id: 0,
-        name: "Tu Vieja2",
-        age: 52,
-        description: "La puta esa2", 
-        price: "Un agua2"
-    },
-    {
-        id: 1,
-        name: "Tu Vieja2",
-        age: 52,
-        description: "La puta esa2", 
-        price: "Un agua2"
-    },
-    {
-        id: 2,
-        name: "Tu Vieja3",
-        age: 53,
-        description: "La puta esa3", 
-        price: "Un agua3"
-    }
-]
-
-server.get("/falopa", (request, response) => {
-    response.send("Hola mundo la re concha de tu hermana");
-});
-
-server.get("/tuvieja", (request, response) => {
-    response.send("<h1>Hola mundo la re concha de tu vieja</h1>");
-});
-
-server.get("/elpibe", (request, response) => {
-    response.send(alumno);
-});
 
 server.get("/students", (request, response) => {
     response.send(students);
---
+++ project/Students.js
@@ -0,0 +1,20 @@
+export const students = [
+    {
+        id: 0,
+        name: "Jason",
+        age: 13,
+        graduated: false
+    },
+    {
+        id: 1,
+        name: "Carl",
+        age: 15,
+        graduated: false
+    },
+    {
+        id: 2,
+        name: "Mark",
+        age: 17,
+        graduated: true
+    }
+]
