318eab2322b9723598691ce663d130042d56bd00
Ignacio Fernandez Lemos <ignacio.f.l@hotmail.com>
2023-12-08 15:35:49 -0300

    refact: add list element finding

--- project/App.js
+++ project/App.js
@@ -1,13 +1,31 @@
 import express from "express"
 
 const server = express();
+server.use(express.urlencoded({extended: true}));
 
-const alumno = {
-    name: "Tu Vieja",
-    age: 5,
-    description: "La puta esa", 
-    price: "Un agua"
-}
+const students = [
+    {
+        id: 0,
+        name: "Tu Vieja2",
+        age: 52,
+        description: "La puta esa2", 
+        price: "Un agua2"
+    },
+    {
+        id: 1,
+        name: "Tu Vieja2",
+        age: 52,
+        description: "La puta esa2", 
+        price: "Un agua2"
+    },
+    {
+        id: 2,
+        name: "Tu Vieja3",
+        age: 53,
+        description: "La puta esa3", 
+        price: "Un agua3"
+    }
+]
 
 server.get("/falopa", (request, response) => {
     response.send("Hola mundo la re concha de tu hermana");
@@ -19,6 +37,16 @@ server.get("/tuvieja", (request, response) => {
 
 server.get("/elpibe", (request, response) => {
     response.send(alumno);
+});
+
+server.get("/students", (request, response) => {
+    response.send(students);
+});
+
+server.get("/students/:id/", (request, response) => {
+    const { id } = request.params;
+    const element = students.find(item => item.id === parseInt(id));
+    response.send(element);
 });
 
 const port = "8080";
