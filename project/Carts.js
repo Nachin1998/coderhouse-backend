d3ac3b1b3d9636b9038544938b2bec95fc98a9fc
Ignacio Fernandez Lemos <ignacio.f.l@hotmail.com>
2023-12-09 17:08:39 -0300

    refact: replace lists with json files

--- project/Carts.js
+++ project/Carts.js
@@ -1,76 +1,72 @@
 import { Router } from "express";
+import fs from 'fs/promises';
 
 const cartRouter = Router();
-const carts = [
-    {
-        id: 1,
-        products: [
-            {
-                product: 1,
-                quantity: 5
-            },
-            {
-                product: 2,
-                quantity: 2
-            }
-        ]
-    },
-    {
-        id: 2,
-        products: [
-            {
-                product: 3,
-                quantity: 1
-            },
-            {
-                product: 9,
-                quantity: 1
-            }
-        ]
-    },
-];
+const CARTS_FILE = 'carts.json';
 
-cartRouter.get("/:id", (request, response) => {
-    const { id } = request.params;
-    const element = carts.find(item => item.id === parseInt(id));
-
-    if(!element){
-        return response.send("No cart of id " + id + " found");
+async function readCarts() {
+    try {
+        const data = await fs.readFile(CARTS_FILE, 'utf8');
+        return JSON.parse(data);
+    } catch (err) {
+        console.error('Error reading file:', err);
+        return [];
     }
-    
-    response.send(element.products);
+}
+
+async function writeCarts(carts) {
+    try {
+        await fs.writeFile(CARTS_FILE, JSON.stringify(carts, null, 2), 'utf8');
+    } catch (err) {
+        console.error('Error writing file:', err);
+    }
+}
+
+cartRouter.get("/:id", async (request, response) => {
+    const { id } = request.params;
+    const carts = await readCarts();
+    const cart = carts.find(item => item.id === parseInt(id));
+
+    if (!cart) {
+        return response.status(404).send("No cart of id " + id + " found");
+    }
+
+    response.send(cart.products);
 });
 
-cartRouter.get("/", (request, response) => {
+cartRouter.get("/", async (request, response) => {
+    const carts = await readCarts();
     response.send(carts);
 });
 
-cartRouter.post("/", (request, response) => {
-    const cart = request.body;
+cartRouter.post("/", async (request, response) => {
+    const newCart = request.body;
+    const carts = await readCarts();
     const id = carts.length + 1;
-    carts.push({...cart, id});
+    carts.push({ ...newCart, id });
+    await writeCarts(carts);
     response.send("Cart added.");
 });
 
-cartRouter.post("/:cartId/products/:productId", (request, response) => {
-    const {cartId, productId} = request.params;
-
+cartRouter.post("/:cartId/products/:productId", async (request, response) => {
+    const { cartId, productId } = request.params;
+    const carts = await readCarts();
     const requestedCart = carts.find(item => item.id === parseInt(cartId));
 
-    if(!requestedCart){
-        return response.send("No cart of id " + cartId + " found");
+    if (!requestedCart) {
+        return response.status(404).send("No cart of id " + cartId + " found");
     }
 
     const productIndex = requestedCart.products.findIndex(item => item.product === parseInt(productId));
-    
-    if(productIndex === -1){
-        requestedCart.products.push({product: parseInt(productId), quantity: 1});
-        response.send("Product not found. Adding new product to cart");
-    }
-    else{
+
+    if (productIndex === -1) {
+        requestedCart.products.push({ product: parseInt(productId), quantity: 1 });
+    } else {
         requestedCart.products[productIndex].quantity++;
-        response.send("Updated cart");
     }
+
+    await writeCarts(carts);
+    response.send("Updated cart");
 });
 
 export default cartRouter;
