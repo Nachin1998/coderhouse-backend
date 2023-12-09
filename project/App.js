f910b5cf6a1faffb057ce43df37a87aa3fcc2321
Ignacio Fernandez Lemos <ignacio.f.l@hotmail.com>
2023-12-09 16:34:08 -0300

    refact: add working carts

--- project/App.js
+++ project/App.js
@@ -1,77 +1,12 @@
 import express from "express"
-import {students} from "./Students.js"
+import cartRouter from "./Carts.js"
+import productsRouter from "./Products.js"
 
 const server = express();
 server.use(express.json());
 server.use(express.urlencoded({extended: true}));
-
-let users = [];
-server.post("/idk", (request, response) => {
-    const user = request.body;
-    const id = users.length + 1;
-    users.push({...user, id});
-    response.send("tamo joya");
-});
-
-server.get("/idk", (request, response) => {
-    response.send(users);
-});
-
-server.put("/idk/:id", (request, response) => {
-    const {id} = request.params;
-
-    if (!users.some(item => item.id === parseInt(id))){
-        response.send({error:"Brokie " + id});
-    }
-    
-    const bodyUser = request.body;
-    const newUsers = users.map(user => {
-        if(user.id === parseInt(id)){
-            return {
-                ...user,
-                ...bodyUser,
-                id: parseInt(id)
-            }
-        }
-
-        return user;
-    });
-    
-    response.send("all good");
-    users = newUsers;
-});
-
-server.delete("/idk/:id", (request, response) => {
-    const{id} = request.params;
-    users = users.filter(user => user.id !== parseInt(id));
-    response.send(id + " deleted");
-});
-
-server.get("/students", (request, response) => {
-    response.send(students);
-});
-
-server.get("/students/:id/", (request, response) => {
-    const { id } = request.params;
-    const element = students.find(item => item.id === parseInt(id));
-    response.send(element);
-});
-
-server.get("/querytest", (request, response) => {
-    const { id, age } = request.query;
-    const element = students.find(item => {
-        const idMatches = item.id === parseInt(id);
-        const ageMatches = !age ? true : item.age === parseInt(age);
-        return idMatches && ageMatches;
-    });
-
-    if(!element){
-        response.send("No such student");
-    }
-    else{
-        response.send(element);
-    }
-});
+server.use("/api/carts", cartRouter);
+server.use("/api/products", productsRouter);
 
 const port = "8080";
 server.listen(port, () => {
---
+++ project/Carts.js
@@ -0,0 +1,81 @@
+import { Router } from "express";
+
+const cartRouter = Router();
+const carts = [
+    {
+        id: 1,
+        products: [
+            {
+                product: 1,
+                quantity: 5
+            },
+            {
+                product: 2,
+                quantity: 2
+            }
+        ]
+    },
+    {
+        id: 2,
+        products: [
+            {
+                product: 3,
+                quantity: 1
+            },
+            {
+                product: 9,
+                quantity: 1
+            }
+        ]
+    },
+];
+
+cartRouter.get("/:id", (request, response) => {
+    const { id } = request.params;
+    const element = carts.find(item => item.id === parseInt(id));
+
+    if(!element){
+        return response.send("No cart of id " + id + " found");
+    }
+    
+    response.send(element.products);
+});
+
+cartRouter.get("/", (request, response) => {
+    response.send(carts);
+});
+
+cartRouter.post("/", (request, response) => {
+    const cart = request.body;
+    const id = carts.length + 1;
+    carts.push({...cart, id});
+    response.send("Cart added.");
+});
+
+cartRouter.post("/:cartId/products/:productId", (request, response) => {
+    const {cartId, productId} = request.params;
+
+    const requestedCart = carts.find(item => item.id === parseInt(cartId));
+
+    if(!requestedCart){
+        return response.send("No cart of id " + cartId + " found");
+    }
+    
+    if (requestedCart.products.some(item => item.product === parseInt(productId))) {
+        for (let i = 0; i < requestedCart.products.length; i++) {
+            const element = requestedCart.products[i];
+            
+            if (element.product === parseInt(productId)){
+                element.quantity++;
+                break;
+            }
+        }
+    }
+    else{
+        requestedCart.products.push({product: parseInt(productId), quantity: 1});
+    }
+
+    response.send("Updated cart");
+});
+
+export default cartRouter;
